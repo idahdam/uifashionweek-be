330a745cc8ea20acd232400e5dcef163c160454c
Hadi <nasshitell@gmail.com>
2021-09-06 17:40:45 +0700

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4b395d01bcd0754912d6fcbdb9aa6810'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1c688a59-a860-43ca-9661-bd5cdd6ce242'
+};
